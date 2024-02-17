6ce3f3e1fcd0fc7ef9e76d882b08c2f93670e39e
DEXE <77177483+DEX-E@users.noreply.github.com>
2024-02-17 23:56:40 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import fortnitepy
+
+bot = fortnitepy.Client(
+  auth=fortnitepy.AuthorizationCodeAuth(
+    code=input("Code: ")
+  )
+)
+
+@bot.command()
+async def cid(ctx,skin)
+  await bot.party.me.set_outfit(cid)
+  await ctx.send(skin)
+
+bot.run()
